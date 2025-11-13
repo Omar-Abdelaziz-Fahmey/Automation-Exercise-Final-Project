22c234010da02f4dbb3689a88e468056cc1e9b9c
Omar-Abdelaziz-Fahmey <omar.ragh37@gmail.com>
2025-11-13 10:17:11 +0200

    ProductDetailsPage

---
+++ src/test/java/automationexercices/tests/ui/ProductDetailsTest.java
@@ -0,0 +1,62 @@
+package automationexercices.tests.ui;
+
+import automationexercices.drivers.GUIDriver;
+import automationexercices.pages.ProductsPage;
+import automationexercices.pages.components.NavigationBarComponent;
+import automationexercices.tests.BaseTest;
+import automationexercices.utils.dataReader.JsonReader;
+import io.qameta.allure.*;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+
+@Epic("Automation Exercise")
+@Feature("UI User Management")
+@Story("Product Details")
+@Severity(SeverityLevel.CRITICAL)
+@Owner("Omar")
+public class ProductDetailsTest extends BaseTest {
+
+    @Test
+    public void verifyProductDetailsWithoutLoginTC() {
+        new ProductsPage(driver).navigate()
+                .clickOnViewProduct(testData.getJsonData("product.name"))
+                .verifyProductDetails(
+                        testData.getJsonData("product.name"),
+                        testData.getJsonData("product.price")
+                );
+    }
+
+    @Test
+    public void verifyReviewMessageWithoutLoginTC() {
+        new ProductsPage(driver).navigate()
+                .clickOnViewProduct(testData.getJsonData("product.name"))
+                .addReview(
+                        testData.getJsonData("review.name")
+                        ,testData.getJsonData("review.email")
+                        ,testData.getJsonData("review.review")
+                )
+                .verifyReviewMsg(testData.getJsonData("messages.review"));
+    }
+
+
+    @BeforeClass
+    protected void preCondition() {
+        testData = new JsonReader("products-details-data");
+    }
+
+
+    //Configurations
+    @BeforeMethod
+    public void setUp() {
+        driver = new GUIDriver();
+        new NavigationBarComponent(driver).navigate();
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.quitDriver();
+    }
+}
