c522138861a7ed0a5b4bb4a41e9c063b1b41d1a3
Omar-Abdelaziz-Fahmey <omar.ragh37@gmail.com>
2025-11-13 10:17:53 +0200

    ProductPage

---
+++ src/test/java/automationexercices/tests/ui/ProductsTest.java
@@ -0,0 +1,66 @@
+package automationexercices.tests.ui;
+
+import automationexercices.drivers.GUIDriver;
+import automationexercices.pages.ProductsPage;
+import automationexercices.pages.components.NavigationBarComponent;
+import automationexercices.tests.BaseTest;
+import automationexercices.utils.TimeManager;
+import automationexercices.utils.dataReader.JsonReader;
+import io.qameta.allure.*;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+
+@Epic("Automation Exercise")
+@Feature("UI Product Management")
+@Story("Product Management")
+@Severity(SeverityLevel.CRITICAL)
+@Owner("Omar")
+public class ProductsTest extends BaseTest {
+    String timestamp = TimeManager.getSimpleTimestamp();
+
+
+    @Test
+    @Description("Search for a product and validate its details")
+    public void searchForProductAndValidateDetailsTest() {
+        new ProductsPage(driver).navigate()
+                .searchProduct(testData.getJsonData("searchedProduct.name"))
+                .validateProductDetails(
+                        testData.getJsonData("searchedProduct.name"),
+                        testData.getJsonData("searchedProduct.price")
+                );
+    }
+    @Test
+    @Description("Add product to cart without logging in")
+    public void addToCartWithoutLoggingInTest() {
+        new ProductsPage(driver).navigate()
+                .clickOnAddToCart(testData.getJsonData("product1.name"))
+                .validateProductAddedToCart(
+                        testData.getJsonData("messages.cartAdded")
+                );
+    }
+
+
+
+
+    @BeforeClass
+    protected void preCondition() {
+        testData = new JsonReader("products-data");
+    }
+
+
+    //Configurations
+    @BeforeMethod
+    public void setUp() {
+        driver = new GUIDriver();
+        new NavigationBarComponent(driver).navigate();
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.quitDriver();
+    }
+
+}
