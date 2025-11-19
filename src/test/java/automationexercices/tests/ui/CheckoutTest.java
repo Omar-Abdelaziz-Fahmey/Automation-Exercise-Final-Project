c290c9a6d68aabb79f158d74a8029d5707d2c93d
Omar-Abdelaziz-Fahmey <omar.ragh37@gmail.com>
2025-11-19 20:11:04 +0200

    Checkout Test

---
+++ src/test/java/automationexercices/tests/ui/CheckoutTest.java
@@ -0,0 +1,129 @@
+package automationexercices.tests.ui;
+
+import automationexercices.apis.UserManagementAPI;
+import automationexercices.drivers.GUIDriver;
+import automationexercices.pages.CartPage;
+import automationexercices.pages.ProductsPage;
+import automationexercices.pages.SignupLoginPage;
+import automationexercices.tests.BaseTest;
+import automationexercices.utils.TimeManager;
+import automationexercices.utils.dataReader.JsonReader;
+import io.qameta.allure.*;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+@Epic("Checkout Management")
+@Feature("UI Checkout Management")
+@Story("Checkout Management")
+@Severity(SeverityLevel.CRITICAL)
+@Owner("Omar")
+
+public class CheckoutTest extends BaseTest {
+    String timestamp = TimeManager.getSimpleTimestamp();
+
+    @Test
+    public void registerNewAccount() {
+        new UserManagementAPI().createRegisterUserAccount(
+                        testData.getJsonData("name"),
+                        testData.getJsonData("email") + timestamp  + "@gmail.com",
+                        testData.getJsonData("password"),
+                        testData.getJsonData("titleMale"),
+                        testData.getJsonData("day"),
+                        testData.getJsonData("month"),
+                        testData.getJsonData("year"),
+                        testData.getJsonData("firstName"),
+                        testData.getJsonData("lastName"),
+                        testData.getJsonData("companyName"),
+                        testData.getJsonData("address1"),
+                        testData.getJsonData("address2"),
+                        testData.getJsonData("country"),
+                        testData.getJsonData("state"),
+                        testData.getJsonData("city"),
+                        testData.getJsonData("zipcode"),
+                        testData.getJsonData("mobileNumber")
+                )
+                .verifyUserCreatedSuccessfully();
+    }
+    @Test(dependsOnMethods = "registerNewAccount")
+    public void loginToAccount() {
+        new SignupLoginPage(driver).navigate()
+                .enterLoginEmail(testData.getJsonData("email") + timestamp + "@gmail.com")
+                .enterLoginPassword(testData.getJsonData("password"))
+                .clickLoginButton()
+                .navigationBar
+                .verifyLoggedInUserName(testData.getJsonData("name"));
+    }
+    @Test(dependsOnMethods = {"loginToAccount","registerNewAccount"})
+    public void addProductToCart() {
+        new ProductsPage(driver)
+                .navigate()
+                .clickOnAddToCart(testData.getJsonData("product.name"))
+                .validateProductAddedToCart(testData.getJsonData("messages.cartAdded"))
+                .clickOnViewCart()
+                .verifyProductDetailsOnCart(
+                        testData.getJsonData("product.name"),
+                        testData.getJsonData("product.price"),
+                        testData.getJsonData("product.quantity"),
+                        testData.getJsonData("product.total")
+                );
+    }
+    @Test(dependsOnMethods ={"addProductToCart","loginToAccount","registerNewAccount"} )
+    public void checkout() {
+        new CartPage(driver)
+                .clickOnProceedToCheckout()
+                .verifyDeliveryAddress(
+                        testData.getJsonData("titleMale"),
+                        testData.getJsonData("firstName"),
+                        testData.getJsonData("lastName"),
+                        testData.getJsonData("companyName"),
+                        testData.getJsonData("address1"),
+                        testData.getJsonData("address2"),
+                        testData.getJsonData("city"),
+                        testData.getJsonData("state"),
+                        testData.getJsonData("zipcode"),
+                        testData.getJsonData("country"),
+                        testData.getJsonData("mobileNumber")
+                )
+                .verifyBillingAddress(
+                        testData.getJsonData("titleMale"),
+                        testData.getJsonData("firstName"),
+                        testData.getJsonData("lastName"),
+                        testData.getJsonData("companyName"),
+                        testData.getJsonData("address1"),
+                        testData.getJsonData("address2"),
+                        testData.getJsonData("city"),
+                        testData.getJsonData("state"),
+                        testData.getJsonData("zipcode"),
+                        testData.getJsonData("country"),
+                        testData.getJsonData("mobileNumber")
+                );
+    }
+
+    @Test(dependsOnMethods = {"checkout","loginToAccount","registerNewAccount"})
+    public void deleteAccountAsPostCondition() {
+        new UserManagementAPI()
+                .deleteUserAccount( testData.getJsonData("email") + timestamp + "@gmail.com",
+                        testData.getJsonData("password"
+                        ))
+                .verifyUserDeletedSuccessfully();
+    }
+
+    //Configurations
+    @BeforeClass
+    protected void setUp() {
+        testData = new JsonReader("checkout-data");
+        driver = new GUIDriver();
+    }
+
+
+    @AfterClass
+    public void tearDown() {
+        driver.quitDriver();
+    }
+
+
+
+
+
+}
