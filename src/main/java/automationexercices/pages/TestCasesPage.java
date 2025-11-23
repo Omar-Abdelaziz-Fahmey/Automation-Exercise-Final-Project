6f79eb8083f45959539413e44c6bf24bb02e3d93
Omar-Abdelaziz-Fahmey <omar.ragh37@gmail.com>
2025-11-23 09:46:09 +0200

    TestCasesPage

--- src/main/java/automationexercices/pages/TestCasesPage.java
+++ src/main/java/automationexercices/pages/TestCasesPage.java
@@ -1,10 +1,38 @@
 package automationexercices.pages;
 
 import automationexercices.drivers.GUIDriver;
+import automationexercices.utils.dataReader.PropertyReader;
+import automationexercices.utils.logs.LogsManager;
+import io.qameta.allure.Step;
+import org.openqa.selenium.By;
 
 public class TestCasesPage {
 
+    private final GUIDriver driver;
     public TestCasesPage(GUIDriver driver){
-
+        this.driver= driver;
     }
+
+    //variables
+    String testCasesEndpoint = "/test_cases";
+
+
+    //locators
+    By testCasesLabel = new By.ByCssSelector("div>h2>b");
+
+    //actions
+    @Step("Navigate to Test Cases page")
+    public TestCasesPage navigate() {
+        driver.browser().navigateTo(PropertyReader.getProperty("baseUrlWeb")  + testCasesEndpoint);
+        return this;
+    }
+
+    //validations
+    @Step("Validate Test Cases page is displayed")
+    public TestCasesPage validateTestCasesPageIsDisplayed() {
+        LogsManager.info("Validating Test Cases page is displayed");
+        driver.verification().isElementVisible(testCasesLabel);
+        return this;
+    }
+
 }
