2e9f3444c5d560850f4135110e052adbe119d1ee
Omar-Abdelaziz-Fahmey <omar.ragh37@gmail.com>
2025-11-29 18:13:36 +0200

    Add critical negative test cases for user registration scenarios

--- src/test/java/automationexercices/tests/ui/RegisterTest.java
+++ src/test/java/automationexercices/tests/ui/RegisterTest.java
@@ -64,7 +64,7 @@ public class RegisterTest extends BaseTest {
     @Test(groups = {"regression", "negative", "register"})
     @Story("User Registration - Negative Scenarios")
     @Description("Verify that appropriate error message is displayed when attempting to register with an already existing email address")
-    @Severity(SeverityLevel.NORMAL)
+    @Severity(SeverityLevel.CRITICAL)
     public void verifyErrorMessageWhenRegisteringWithExistingEmailTC() {
         new UserManagementAPI().createRegisterUserAccount(
                         testData.getJsonData("name"),
@@ -105,7 +105,7 @@ public class RegisterTest extends BaseTest {
     @Test(groups = { "regression", "negative", "register" })
     @Story("User Registration - Negative Scenarios")
     @Description("Verify that appropriate error message is displayed when attempting to register with an invalid email address")
-    @Severity(SeverityLevel.NORMAL)
+    @Severity(SeverityLevel.CRITICAL)
     public void verifyErrorMessageWhenRegisteringWithInvalidEmailTC() {
         new SignupLoginPage(driver).navigate()
                 .enterSignupName(testData.getJsonData("name"))
@@ -118,8 +118,8 @@ public class RegisterTest extends BaseTest {
 
     @Test(groups = { "regression", "negative", "register" })
     @Story("User Registration - Negative Scenarios")
-    @Description("Verify that account is not created when mandatory fields are missing")
-    @Severity(SeverityLevel.NORMAL)
+    @Description("Verify that account is not created when Password field is missing")
+    @Severity(SeverityLevel.CRITICAL)
     public void verifyErrorMessageWhenRegisteringWithMissingFieldsTC() {
         new SignupLoginPage(driver).navigate()
                 .enterSignupName(testData.getJsonData("name"))
@@ -146,6 +146,84 @@ public class RegisterTest extends BaseTest {
                 .clickCreateAccountButton()
                 .verifyOnSignupPage(); // Verify we are still on the signup page
     }
+
+    @Test(groups = { "regression", "negative", "register" })
+    @Story("User Registration - Negative Scenarios")
+    @Description("Verify that account is not created when Name fields are missing")
+    @Severity(SeverityLevel.CRITICAL)
+    public void verifyErrorMessageWhenRegisteringWithMissingNameTC() {
+        new SignupLoginPage(driver).navigate()
+                .enterSignupName(testData.getJsonData("name"))
+                .enterSignupEmail(testData.getJsonData("email") + timestamp + "@gmail.com")
+                .clickSignupButton();
+
+        new SignupPage(driver)
+                .chooseTitle(testData.getJsonData("titleMale"))
+                .enterPassword(testData.getJsonData("password"))
+                .selectDateOfBirth(
+                        testData.getJsonData("day"),
+                        testData.getJsonData("month"),
+                        testData.getJsonData("year"))
+                // Skip first and last name to trigger missing field error
+                .enterCompany(testData.getJsonData("companyName"))
+                .enterAddress1(testData.getJsonData("address1"))
+                .enterAddress2(testData.getJsonData("address2"))
+                .selectCountry(testData.getJsonData("country"))
+                .enterState(testData.getJsonData("state"))
+                .enterCity(testData.getJsonData("city"))
+                .enterZipcode(testData.getJsonData("zipcode"))
+                .enterMobileNumber(testData.getJsonData("mobileNumber"))
+                .clickCreateAccountButton()
+                .verifyOnSignupPage();
+
+
+    }
+    @Test(groups = { "regression", "negative", "register" })
+    @Story("User Registration - Negative Scenarios")
+    @Description("Verify that account is not created with Invalid Zipcode")
+    @Severity(SeverityLevel.CRITICAL)
+    public void verifyErrorMessageWhenRegisteringWithInvalidZipcodeTC() {
+
+        new SignupLoginPage(driver).navigate()
+                .enterSignupName(testData.getJsonData("name"))
+                .enterSignupEmail(testData.getJsonData("email") + timestamp + "@gmail.com")
+                .clickSignupButton();
+
+        new SignupPage(driver)
+                .chooseTitle(testData.getJsonData("titleMale"))
+                .enterPassword(testData.getJsonData("password"))
+                .selectDateOfBirth(
+                        testData.getJsonData("day"),
+                        testData.getJsonData("month"),
+                        testData.getJsonData("year"))
+                .enterFirstName(testData.getJsonData("firstName"))
+                .enterLastName(testData.getJsonData("lastName"))
+                .enterCompany(testData.getJsonData("companyName"))
+                .enterAddress1(testData.getJsonData("address1"))
+                .enterAddress2(testData.getJsonData("address2"))
+                .selectCountry(testData.getJsonData("country"))
+                .enterState(testData.getJsonData("state"))
+                .enterCity(testData.getJsonData("city"))
+                .enterZipcode(testData.getJsonData("invalidZipcode"))
+                .enterMobileNumber(testData.getJsonData("mobileNumber"))
+                .clickCreateAccountButton()
+                .verifyOnSignupPage();
+
+        // Delete the account if it is registered
+
+
+
+        new UserManagementAPI().deleteUserAccount(
+                        testData.getJsonData("email") + timestamp + "@gmail.com",
+                        testData.getJsonData("password"))
+                .verifyUserDeletedSuccessfully();
+
+
+
+    }
+
+
+
 
 
     //Configurations
